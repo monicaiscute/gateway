64a08b93c382fc7e09710ffc1752105de71031ff
monicaiscute <2339826588@qq.com>
2020-12-16 14:06:37 +0800

    Dockerfile

--- src/main/java/com/GatewayApplication.java
+++ src/main/java/com/GatewayApplication.java
@@ -1,28 +1,15 @@
 package com;
-//import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-import com.alibaba.cloud.nacos.ribbon.NacosRibbonClientConfiguration;
 import com.netflix.client.config.DefaultClientConfigImpl;
 import com.netflix.client.config.IClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.cloud.gateway.filter.RouteToRequestUrlFilter;
-//import org.springframework.context.annotation;
-//import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 /**
  * @author litao
  */
 //@EnableCaching
 @EnableDiscoveryClient
-//@SpringBootApplication(scanBasePackages = {"com.*"},scanBasePackageClasses = com.netflix.client.config.IClientConfig.class)
-//@SpringBootApplication(scanBasePackages={"com.netflix.client.config.IClientConfig"})
-//@RibbonClients(defaultConfiguration = NacosRibbonClientConfiguration.class)
 @SpringBootApplication(scanBasePackages = {"com.*"})
 public class GatewayApplication {
 //    /*跨域处理*/
